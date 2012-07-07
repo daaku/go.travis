4637c061073fdea8ea7f2e7ecb39d7c4a06451fa
Naitik Shah <n@daaku.org>
2012-07-07 07:46:49 +0000

    handle xtest

---
+++ xtest/travis_xtest.go
@@ -0,0 +1,10 @@
+package travis_test
+
+import (
+	_ "github.com/nshah/go.flagconfig" // to check that imports are handled
+	"testing"
+)
+
+func TestMeta(t *testing.T) {
+	t.Log("TestMeta works.")
+}
