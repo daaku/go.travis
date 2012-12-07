f3cb8baf16293eaf96c1e001acc67bb23e7546b2
Naitik Shah <n@daaku.org>
2012-12-06 22:53:00 -0800

    an import that actually gets used

--- xtest/travis_test.go
+++ xtest/travis_test.go
@@ -1,10 +1,14 @@
 package travis_test
 
 import (
-	_ "github.com/daaku/go.flagconfig" // to check that imports are handled
+	"github.com/daaku/go.domain" // to check that xtest imports are handled
 	"testing"
 )
 
 func TestMeta(t *testing.T) {
+	dom, _ := domain.TLD("facebook.com")
+	if dom != "com" { // just need to use domain
+		t.Fatal("Horribly wrong")
+	}
 	t.Log("TestMeta works.")
 }
